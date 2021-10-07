ffc5cdf1b41b125157978a29aa0594a640b020de
creative-style <clodoaldo.neto.116@ufrn.edu.br>
2021-10-07 17:56:43 -0300

    new project structure

---
+++ blog/src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { NavComponent } from './nav/nav.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    NavComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
