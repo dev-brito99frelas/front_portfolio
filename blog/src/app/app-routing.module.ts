7f37512df0998b8db020c6433d17d4958d314bda
Clodoaldo-Neto <clodoaldo.neto.116@ufrn.edu.br>
2021-10-10 22:42:37 -0300

    added the pages who are contained in the app designer

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+/**@description Paginas da app */
+import { BlogComponent } from './pages/blog/blog.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ServicesComponent } from './pages/services/services.component';
+
+
+const routes: Routes = [
+  {path: '',component: HomeComponent, pathMatch: 'full'},
+  {path: 'profile',component: ProfileComponent},
+  {path: 'projects',component: ProjectsComponent},
+  {path: 'services',component: ServicesComponent},
+  {path: 'blog',component: BlogComponent},
+  {path: 'error',component: ErrorComponent},
+  {path: '**',redirectTo: 'error'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- blog/src/app/pages/pages.module.ts
+++ blog/src/app/pages/pages.module.ts
@@ -1,12 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+/**@description componenetes pages */
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { BlogComponent } from './blog/blog.component';
+import { ServicesComponent } from './services/services.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ErrorComponent } from './error/error.component';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    ProjectsComponent,
+    BlogComponent,
+    ServicesComponent,
+    ProfileComponent,
+    ErrorComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HomeComponent,
+    ProjectsComponent,
+    BlogComponent,
+    ServicesComponent,
+    ProfileComponent,
+    ErrorComponent
   ]
 })
 export class PagesModule { }
